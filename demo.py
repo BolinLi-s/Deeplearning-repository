d9d9d768bbdb5546cb84d157da843fa6d45d49d4
libolin <48210500+bolin12@users.noreply.github.com>
2020-12-17 15:41:45 +0800

    aDS

--- demo.py
+++ demo.py
@@ -1,5 +1,6 @@
 import math
 import numpy as np
+
 def ddd(x):
     return -x*math.log(x,2)
 
@@ -7,4 +8,3 @@ a = ddd(0.64) + 2*ddd(0.16) + ddd(0.04)
 b = ddd(0.8) + ddd(0.2)
 b = ddd(8/3)
 print(b)
-
